3276daf2a31fc47b7f303af409c2fa5a0f4a91a8
yewon kim <yewonkim@yewonui-MacBookPro.local>
2024-05-27 19:10:12 +0900

    프리즈마 클라이언트

--- src/app.js
+++ src/app.js
@@ -1,2 +1,14 @@
 import express from "express";
 import "dotenv/config";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const PORT = 3018;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api", []);
+
+app.listen(PORT, () => {
+  console.log(PORT, "포트로 서버가 열렸어요!");
+});
