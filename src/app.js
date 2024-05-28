a1f7b27a69312f2046360d12454ed5129672d8e2
yewon kim <yewonkim@yewonui-MacBookPro.local>
2024-05-28 15:38:31 +0900

    에러처리 미들웨어 연결

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import errorHandlerMiddleware from "./middlewares/error-handler.middleware.js";
 import UsersRouter from "./routers/users.router.js";
 
 const app = express();
@@ -9,6 +10,7 @@ const PORT = 3018;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/auth", [UsersRouter]);
+app.use(errorHandlerMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Home Page!");
--- src/middlewares/error-handler.middleware.js
+++ src/middlewares/error-handler.middleware.js
@@ -1,17 +1,7 @@
-export const errorHandler = (err, req, res, next) => {
+export default function (err, req, res, next) {
+  // 에러를 출력합니다.
   console.error(err);
 
-  // joi에서 발생한 에러 처리
-  if (err.name === "ValidationError") {
-    return res.status(400).json({
-      status: 400,
-      message: err.message,
-    });
-  }
-
-  // 그 밖의 예상치 못한 에러 처리
-  return res.status(500).json({
-    status: 500,
-    message: "예상치 못한 에러가 발생했습니다. 관리자에게 문의해 주세요.",
-  });
-};
+  // 클라이언트에게 에러 메시지를 전달합니다.
+  res.status(500).json({ errorMessage: "서버 내부 에러가 발생했습니다." });
+}
